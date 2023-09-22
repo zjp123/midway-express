0f75aece7977fab419d6cd2d6fa3c02c25d63a87
zhangjunpeng <zhangjunpeng@58.com>
2023-09-22 10:30:39 +0800

    feat: signedCookies 加密cookie

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Inject } from '@midwayjs/core';
 import { Context } from '@midwayjs/express';
+import { ReportMiddleware } from '../middleware/reportMiddleware';
 
 @Controller('/')
 export class HomeController {
   @Inject()
   ctx: Context;
 
-  @Get('/')
+  @Get('/', { middleware: [ReportMiddleware] })
   async home(): Promise<string> {
-    const cookieValue = this.ctx.cookies['test-midkie'];
+    // const cookieValue = this.ctx.cookies['test-midkie'];
+    const cookieValue = this.ctx.signedCookies['test-midkie'];
     console.log(cookieValue, 'cookie');
     return 'Hello Midwayjs!';
   }
